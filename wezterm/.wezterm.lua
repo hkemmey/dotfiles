3946ae6f6f443c20d9e638365ebbb25db3c6cc3a
hkemmey <harrison.kemmey@gmail.com>
2025-02-26 17:54:35 -0500

    window decorations

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,8 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local mux = wezterm.mux
 local config = wezterm.config_builder()
+
+config.window_decorations = "RESIZE"
 
 return config
