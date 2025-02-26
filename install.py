1b560b40a5096469374dcdef721caade95f9d969
hkemmey <harrison.kemmey@gmail.com>
2025-02-25 19:53:29 -0500

    refactor

--- install.py
+++ install.py
@@ -4,23 +4,23 @@ import sys
 HOME = os.getenv("XDG_CONFIG_HOME", os.getenv("HOME"))
 
 def create_link(source: str, to: str):
-    os.symlink(os.path.abspath(source), f"{HOME}/{to}")
+    if os.path.exists(to):
+        os.rename(to, f"{to}.bak") #disable old configs without deleting
+
+    os.symlink(os.path.abspath(source), to)
 
 def remove_link(to: str):
-    if os.path.islink(f"{HOME}/{to}"):
-        os.unlink(f"{HOME}/{to}")
+    if os.path.islink(to):
+        os.unlink(to)
     else:
-        print(f"cannot unlink nonexistant link: {HOME}/{to}")
+        print(f"cannot unlink nonexistant link: {to}")
 
 if __name__ == '__main__':
-    print(HOME)
     
-    links: dict = {
-        "wezterm/.wezterm.lua": ".wezterm.lua",
-        "zed/keymap.json": ".config/zed/keymap.json",
-        "zed/settings.json": ".config/zed/settings.json"
-    }
+    ## create links
+    create_link("wezterm/.wezterm.lua", f"{HOME}/.wezterm.lua")
+    create_link("zed/keymap.json", f"{HOME}/.config/zed/keymap.json")
+    create_link("zed/settings.json", f"{HOME}/.config/zed/settings.json")
 
-    for key in links:
-        print(f"{HOME}/{key}")
+    ## exits
     sys.exit()
