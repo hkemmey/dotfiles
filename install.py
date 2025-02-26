3a8b29d403ba1eb99ff1e1656329a478ba596df9
hkemmey <harrison.kemmey@gmail.com>
2025-02-25 20:25:02 -0500

    add flag to uninstall dotfiles

--- install.py
+++ install.py
@@ -13,17 +13,27 @@ def create_link(source: str, to: str):
 def remove_link(to: str):
     if os.path.islink(to):
         os.unlink(to)
+        if os.path.exists(f"{to}.bak"):
+            os.rename(f"{to}.bak", to)
     else:
         print(f"cannot unlink nonexistant link: {to}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--uninstall", type=bool, default=False, required=False)
+    parser.add_argument("-u", "--uninstall", action="store_true", required=False)
     
-    ## create links
-    create_link("wezterm/.wezterm.lua", f"{HOME}/.wezterm.lua")
-    create_link("zed/keymap.json", f"{HOME}/.config/zed/keymap.json")
-    create_link("zed/settings.json", f"{HOME}/.config/zed/settings.json")
+    args = parser.parse_args()
+    
+    if args.uninstall:
+        ## remove links and rename .bak files
+        remove_link(f"{HOME}/.wezterm.lua")
+        remove_link(f"{HOME}/.config/zed/keymap.json")
+        remove_link(f"{HOME}/.config/zed/settings.json")
+    else:
+        ## create links
+        create_link("wezterm/.wezterm.lua", f"{HOME}/.wezterm.lua")
+        create_link("zed/keymap.json", f"{HOME}/.config/zed/keymap.json")
+        create_link("zed/settings.json", f"{HOME}/.config/zed/settings.json")
 
     ## exits
     sys.exit()
